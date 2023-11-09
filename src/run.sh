219b7dc5b44dc07c51c603465d3b190ff51c26c9
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-11-09 12:30:44 +0100

    wip

--- src/run.sh
+++ src/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-export PYTHONPATH=${{ env.CDOT_DIR }}
+export PYTHONPATH=$CDOT_DIR
 
 # create gene information JSON file
 EMAIL=manuel.holtgrewe@bih-charite.de \
-bash ${{ env.CDOT_DIR }}/generate_transcript_data/gene_info.sh
+bash $CDOT_DIR/generate_transcript_data/gene_info.sh
 
 # downlooad data, prepare cdot JSON files, and create seqrepos
 bash src/prepare.sh grch38
