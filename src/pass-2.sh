31a2dcbc7c2bc2d2760bc333f3c642a0772634df
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-03-06 10:51:08 +0100

    feat: use mehari ghcr.io docker image and bump to v0.25.1

--- src/pass-2.sh
+++ src/pass-2.sh
@@ -10,16 +10,21 @@ set -x
 
 mkdir -p $DATA_DIR/pass-2
 
-mehari db create \
-    $(if [[ "$GENOME_RELEASE" == grch37 ]]; then \
-        echo --path-mane-txs-tsv; \
-        echo $DATA_DIR/tmp/mane-txs.tsv; \
-    fi) \
-    --path-out $DATA_DIR/pass-2/txs.bin.zst \
-    --path-seqrepo-instance $DATA_DIR/seqrepo/master \
-    --path-cdot-json $DATA_DIR/tmp/$GENOME_RELEASE/$CDOT_FILENAME \
-    --path-cdot-json $SCRIPT_DIR/../data/cdot-0.2.23.ensembl.chrMT.$GENOME_RELEASE.gff3.json \
-    --genome-release $GENOME_RELEASE
+sudo docker run \
+    --volume "$DATA_DIR:$DATA_DIR:rw" \
+    --volume "$(readlink -f $SCRIPT_DIR/..):$(readlink -f $SCRIPT_DIR/..):ro" \
+    ghcr.io/varfish-org/mehari:${MEHARI_VERSION} \
+    exec /usr/local/bin/mehari \
+        db create \
+        $(if [[ "$GENOME_RELEASE" == grch37 ]]; then \
+            echo --path-mane-txs-tsv; \
+            echo $DATA_DIR/tmp/mane-txs.tsv; \
+        fi) \
+        --path-out $DATA_DIR/pass-2/txs.bin.zst \
+        --path-seqrepo-instance $DATA_DIR/seqrepo/master \
+        --path-cdot-json $DATA_DIR/tmp/$GENOME_RELEASE/$CDOT_FILENAME \
+        --path-cdot-json $SCRIPT_DIR/../data/cdot-0.2.23.ensembl.chrMT.$GENOME_RELEASE.gff3.json \
+        --genome-release $GENOME_RELEASE
 cd $DATA_DIR/pass-2
 
 # Ensure that the output can be decompressed.
