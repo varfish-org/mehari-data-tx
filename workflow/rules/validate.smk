b4b9959134a0fc79e81f37fa01694c44a6df286d
Till Hartmann <till.hartmann@bih-charite.de>
2024-06-20 15:06:09 +0200

    add simple validation script + stats

---
+++ workflow/rules/validate.smk
@@ -0,0 +1,40 @@
+rule dump_mehari_db:
+    input:
+        tx_db="results/mehari/{alias}/{seqrepo}/txs.bin.zst",
+        # report="results/mehari/{alias}/{seqrepo}/txs.bin.zst.report.jsonl",
+    output:
+        db_yaml="results/mehari/{alias}/{seqrepo}/txs.bin.zst.yaml.gz",
+    # conda:
+    #     "../envs/mehari.yaml"
+    log:
+        "logs/mehari/{alias}/{seqrepo}/dump.log",
+    shell:
+        """
+        mehari db dump --path-db {input.tx_db} | gzip -c > {output.db_yaml} 2> {log}
+        """
+
+
+rule mehari_transcript_ids:
+    input:
+        db_yaml="results/mehari/{alias}/{seqrepo}/txs.bin.zst.yaml.gz",
+    output:
+        tx_ids="results/mehari/{alias}/{seqrepo}/txs.bin.zst.transcript_ids.txt",
+    log:
+        "logs/mehari/{alias}/{seqrepo}/transcript_ids.log",
+    conda:
+        "../envs/jq.yaml"
+    shell:
+        """yq -r '.txDb.transcripts[].id' <(pigz -dc {input.db_yaml}) > {output.tx_ids} 2> {log}"""
+
+
+rule check_mehari_db:
+    input:
+        kept="results/mehari/{alias}/{seqrepo}/txs.bin.zst.transcript_ids.txt",
+        db_discarded="results/mehari/{alias}/{seqrepo}/txs.bin.zst.report.jsonl",
+        cdot="results/transcripts/cdot/{alias}.hgnc.json.gz",
+    output:
+        stats="results/mehari/{alias}/{seqrepo}/txs.bin.zst.stats",
+    conda:
+        "../envs/datastuff.yaml"
+    script:
+        "../scripts/check_mehari_db.py"
---
+++ workflow/scripts/check_mehari_db.py
@@ -0,0 +1,67 @@
+import json
+import polars as pl
+import gzip
+
+
+def cdot_transcript_ids() -> set[str]:
+    with gzip.open(snakemake.input.cdot, "r") as f:
+        cdot = json.load(f)
+        cdot_transcript_ids = set(cdot["transcripts"].keys())
+        return cdot_transcript_ids
+
+
+def discarded_transcripts() -> pl.DataFrame:
+    return (
+        pl.scan_ndjson(snakemake.input.db_discarded, ignore_errors=True)
+        .filter(pl.col("type") == "Discard")
+        .unnest("value")
+        .collect()
+    )
+
+
+def discarded_transcript_ids(discarded) -> set[str]:
+    discarded_transcript_ids = (
+        discarded.select(pl.col("id"))
+        .unnest("id")
+        .filter(pl.col("type").is_in(["TxId"]))
+        .select(pl.col("value"))
+    )
+    return set(discarded_transcript_ids.to_series().to_list())
+
+
+def kept_transcript_ids() -> set[str]:
+    with open(snakemake.input.kept, "r") as f:
+        return set(l.strip() for l in f)
+
+
+cdot_tx_ids = cdot_transcript_ids()
+
+discarded = discarded_transcripts()
+stats: pl.DataFrame = (
+    discarded.group_by(["source", "kind", "reason"])
+    .len()
+    .sort(["source", "kind", "len"])
+)
+stats.write_csv(snakemake.output.stats, include_header=True, separator="\t")
+
+discarded_tx_ids = discarded_transcript_ids(discarded)
+
+kept_tx_ids = kept_transcript_ids()
+
+print(f"Number of transcripts in cdot: {len(cdot_tx_ids)}")
+print(f"Number of discarded transcripts: {len(discarded_tx_ids)}")
+print(f"Number of kept transcripts: {len(kept_tx_ids)}")
+
+num_kept = len(cdot_tx_ids & kept_tx_ids)
+num_discarded = len(cdot_tx_ids & discarded_tx_ids)
+assert num_kept + num_discarded == len(cdot_tx_ids)
+
+for transcript_id in cdot_tx_ids:
+    tx_discarded = transcript_id in discarded_tx_ids
+    tx_kept = transcript_id in kept_tx_ids
+    if tx_discarded and tx_kept:
+        raise ValueError(f"Tx {transcript_id} is both kept and discarded")
+    if not tx_discarded and not tx_kept:
+        raise ValueError(f"Tx {transcript_id} is neither kept nor discarded")
+
+print(f"Kept: {num_kept}, Discarded: {num_discarded}")
