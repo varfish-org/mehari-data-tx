ccc77486ece5209e50383295dfe88fe6c91920e0
Till Hartmann <till.hartmann@bih-charite.de>
2024-06-27 11:21:12 +0200

    add ratelimiting to fasta fetching script for good measure

--- workflow/scripts/fetch_missing_sequences.py
+++ workflow/scripts/fetch_missing_sequences.py
@@ -1,14 +1,29 @@
 from contextlib import redirect_stderr
+from datetime import timedelta
+
 import bioutils.seqfetcher
+from ratelimit import limits, sleep_and_retry
+
+
+@sleep_and_retry
+@limits(calls=10, period=timedelta(seconds=1).total_seconds())
+def get_fasta(accession: str) -> str | None:
+    try:
+        seq = bioutils.seqfetcher.fetch_seq(accession)
+        if seq:
+            return f">{accession}\n{seq}"
+    except Exception as e:
+        print(f"Failed to fetch {accession}: {e}", file=sys.stderr)
+    return None
 
 
 def main(missing_ids: str, file):
     with open(missing_ids, "r") as f:
         for line in f.readlines():
             accession = line.strip()
-            seq = bioutils.seqfetcher.fetch_seq(accession)
-            if seq:
-                print(f">{accession}\n{seq}", file=file)
+            fasta = get_fasta(accession)
+            if fasta:
+                print(fasta, file=file)
 
 
 with open(snakemake.log[0], "w") as log, redirect_stderr(log):
