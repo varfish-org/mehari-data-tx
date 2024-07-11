16898ebfbbd289f7dd69c756dc5537eb2803a6dd
Till Hartmann <till.hartmann@bih-charite.de>
2024-07-11 11:14:01 +0200

    fix ensembl id lookup script

--- workflow/scripts/lookup_ensembl_ids_for_refseq_ids.py
+++ workflow/scripts/lookup_ensembl_ids_for_refseq_ids.py
@@ -5,22 +5,55 @@ import pandas as pd
 import requests
 from snakemake.script import snakemake
 
+HEADER = {
+    "refseq_mrna": "RefSeq mRNA ID",
+    "ensembl_gene_id": "Gene stable ID",
+    "ensembl_gene_id_version": "Gene stable ID version",
+    "ensembl_transcript_id": "Transcript stable ID",
+    "ensembl_transcript_id_version": "Transcript stable ID version",
+    "hgnc_id": "HGNC ID",
+}
+
+CONTIGS = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "MT",
+    "X",
+    "Y",
+]
+
 
 def main(refseq_ids: set[str]):
     refseq_ids = ",".join(s.rsplit(".", 1)[0] for s in refseq_ids)
-
+    attributes = "".join(f'<Attribute name = "{name}" />' for name in HEADER.keys())
+    contigs = ",".join(CONTIGS)
     xml = rf"""<?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE Query>
-    <Query  virtualSchemaName = "default" formatter = "TSV" header = "1" uniqueRows = "1" count = "" datasetConfigVersion = "0.6" >
-        <Dataset name = "hsapiens_gene_ensembl" interface = "default" >
-            <Filter name = "chromosome_name" value = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,MT,X,Y"/>
-            <Filter name = "refseq_mrna" value = "{refseq_ids}"/>\\
-            <Attribute name = "refseq_mrna" />
-            <Attribute name = "ensembl_gene_id" />\
-            <Attribute name = "ensembl_gene_id_version" />\
-            <Attribute name = "ensembl_transcript_id" />
-            <Attribute name = "ensembl_transcript_id_version" />
-            <Attribute name = "hgnc_id" />
+    <Query virtualSchemaName="default" formatter="TSV" header="1" uniqueRows="1" count="" datasetConfigVersion="0.6">
+        <Dataset name="hsapiens_gene_ensembl" interface="default" >
+            <Filter name="chromosome_name" value="{contigs}"/>
+            <Filter name="refseq_mrna" value="{refseq_ids}"/>\\
+            {attributes}
         </Dataset>
     </Query>"""
 
@@ -37,12 +70,12 @@ with open(snakemake.log[0], "w") as log, redirect_stderr(log):
     if snakemake.wildcards.source.lower() == "refseq":
         response = main(refseq_ids).content.decode()
         try:
-            pd.read_csv(StringIO(response), sep="\t")
+            pd.read_csv(StringIO(response), sep="\t", names=list(HEADER.values()))
         except Exception as e:
             raise ValueError(
                 f"Expected TSV response from biomart, got:\n{response}"
             ) from e
     else:
-        response = "RefSeq mRNA ID\tGene stable ID\tGene stable ID version\tTranscript stable ID\tTranscript stable ID version\tHGNC ID\n"
+        response = "\t".join(HEADER.values()) + "\n"
     with open(snakemake.output.tsv, "wt") as f:
         f.write(response)
